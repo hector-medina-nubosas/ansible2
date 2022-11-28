6ef4d87499ae1c4d2237659b2988f498ed3eb1b0
hector-medina-nubosas <hector.medina@nubosas.com>
2022-11-28 09:00:43 +0100

    f

--- sample-app-2/roles/scheduler/files/update/nginx/update_packages.sh
+++ sample-app-2/roles/scheduler/files/update/nginx/update_packages.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-RESULT=$(ansible-pull -U https://github.com/hector-medina-nubosas/ansible2.git -C nginx-latest sample-app-2/roles/common/)
+RESULT=$(ansible-pull -U https://github.com/hector-medina-nubosas/ansible2.git -C nginx-latest sample-app-2/nginx-server-update.yml)
 echo "$RESULT" >> /root/registro.log
